015d590bfcb6df166d19dad3b9f31ef037efb502
tooblippe <tobie.nortje@gmail.com>
2022-09-19 09:16:39 +0200

    Addition of conditional compilation.

--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,5 @@
-sphinx-build -b html -t SOUTHAFRICA . build/SOUTHAFRICA
-sphinx-build -b html -t BOTSWANA . build/BOTSWANA
-sphinx-build -b html -t AUSTRALIA . build/AUSTRALIA
-sphinx-build -b html -t NEWZEALAND . build/NEWZEALAND
+sphinx-build -b html -t south_africa . build/south_africa
+sphinx-build -b html -t botswana . build/botswana
+sphinx-build -b html -t australia . build/australia
+sphinx-build -b html -t new_zealand . build/new_zealand
+sphinx-build -b html -t default . build/default
