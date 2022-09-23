c874ee73465074168e4df98e8935218df58f2bc6
tooblippe <tobie.nortje@gmail.com>
2022-09-23 08:55:14 +0200

    added Linux build script

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,9 @@
-cp _includefiles\client_default.txt _includefiles\default.txt
-cp images\Company_Logo_Default.png images\Company_Logo.png
-cp images\Screenshot_Default.png images\Company_Screenshot.png
+cp _includefiles/client_default.txt _includefiles/default.txt
+cp images/Company_Logo_Default.png images/Company_Logo.png
+cp images/Screenshot_Default.png images/Company_Screenshot.png
 sphinx-build -b html -t default . build/default
 
-cp _includefiles\client_australia_TIQ.txt _includefiles\default.txt
-cp images\Company_Logo_Australia_TIQ.png images\Company_Logo.png
-cp images\Screenshot_Australia_TIQ.png images\Company_Screenshot.png
+cp _includefiles/client_australia_TIQ.txt _includefiles/default.txt
+cp images/Company_Logo_Australia_TIQ.png images/Company_Logo.png
+cp images/Screenshot_Australia_TIQ.png images/Company_Screenshot.png
 sphinx-build -b html -t australia . build/australia
